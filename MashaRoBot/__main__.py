6ac58cd0270ff33dcefa5c27758e40a680d839a9
gangofbot <85664950+gangofbot@users.noreply.github.com>
2021-06-17 15:46:04 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -74,14 +74,14 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-𝙷𝙴𝚈,my name is Haguromo Otsusuki/n
-𝙸'𝙰𝙼 𝙰𝙽 𝙰𝙽𝙸𝙼𝙴 𝚃𝙷𝙴𝙼𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 𝙼𝙰𝙽𝙰𝙶𝙸𝙽𝙶 𝙱𝙾𝚃! BY SATYA AND EMCEE DEVS 𝙲𝙻𝙸𝙲𝙺` *📚𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂* `𝙱𝚄𝚃𝚃𝙾𝙽 𝙱𝙴𝙻𝙾𝚆 𝚃𝙾 𝙴𝚇𝙿𝙻𝙾𝚁𝙴 𝙼𝙾𝚁𝙴 𝙰𝙱𝙾𝚄𝚃 𝙼𝙴 𝙰𝙽𝙳 𝙼𝚈 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽𝚂 [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
+𝙷𝙴𝚈,my name is ◤Mʀ𝙂𝘼𝙉𝙂𝙎𝙏𝙀𝙍/n
+𝙸'𝙰𝙼 𝙰𝙽 𝙰𝙽𝙸𝙼𝙴 𝚃𝙷𝙴𝙼𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 𝙼𝙰𝙽𝙰𝙶𝙸𝙽𝙶 𝙱𝙾𝚃! BY SATYA AND EMCEE DEVS 𝙲𝙻𝙸𝙲𝙺` *📚𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂* `𝙱𝚄𝚃𝚃𝙾𝙽 𝙱𝙴𝙻𝙾𝚆 𝚃𝙾 𝙴𝚇𝙿𝙻𝙾𝚁𝙴 𝙼𝙾𝚁𝙴 𝙰𝙱𝙾𝚄𝚃 𝙼𝙴 𝙰𝙽𝙳 𝙼𝚈 𝙵𝚄𝙽𝙲𝚃𝙸𝙾𝙽𝚂 [.](https://telegra.ph/file/d03045c496a1fad34fdc0.jpg)
 """
 
 buttons = [
     [
         InlineKeyboardButton(
-            text="➕️ ADD HAGUROMO TO YOUR GROUP ➕️", url="t.me/rikudo_senin_bot?startgroup=true"),
+            text="➕️ ADD HAGUROMO TO YOUR GROUP ➕️", url="t.me/Mrgangbot?startgroup=true"),
     ],
     [
         InlineKeyboardButton(text="ℹ️ABOUT", callback_data="masha_"),
@@ -89,9 +89,9 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="✨Owner✨", url="http://t.me/kid_of_telegram"),
+            text="✨Owner✨", url="http://t.me/Gang_Of_Telegram"),
         InlineKeyboardButton(
-            text="👥SUPPORT👥", url="https://t.me/tz_me_kid"),
+            text="👥SUPPORT👥", url="https://t.me/MRGangsterbot"),
     ],
     [  
         InlineKeyboardButton(text="Thanks to", url="https://t.me/emcee_support"),
@@ -105,7 +105,7 @@ HELP_STRINGS = """
 ༆*Rikudo Senin comes with many special features in it*༆
 ꧁*CHECK ALL BUTTON BELOW TO EXPLORE EVERY COMMANDS OF EMCEE*꧂
 𖣘 *All commands can either be used with* `/` *or* `!`.
-𖣘 *If you facing any issue or find any bugs in any command then you can report it in @Emcee_Support* [.](https://telegra.ph/file/36c57221ea00d88fbcd89.jpg)
+𖣘 *If you facing any issue or find any bugs in any command then you can report it in @Emcee_Support* [.](https://telegra.ph/file/d03045c496a1fad34fdc0.jpg)
 """
 
 
@@ -354,14 +354,14 @@ def Masha_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "masha_":
         query.message.edit_text(
-            text=""" ℹ️ I'm *Haguromo Otsusuki*, a powerful group management bot built to help you manage your group easily.
+            text=""" ℹ️ I'm *◤Mʀ𝙂𝘼𝙉𝙂𝙎𝙏𝙀𝙍*, a powerful group management bot built to help you manage your group easily.
                  ❍ I can restrict users.
                  ❍ I can greet users with customizable welcome messages and even set a group's rules.
                  ❍ I have an advanced anti-flood system.
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Emcee's licensed under the GNU General Public License v3.0_
+                 \n_Mʀ𝙂𝘼𝙉𝙂𝙎𝙏𝙀𝙍's licensed under the GNU General Public License v3.0_
                  Here is the [💾Repository](https://www.github.com/satyanandatripathi/emcee).
                  If you have any question about Rikudo senin, let us know at @satyanandatripathi.""",
             parse_mode=ParseMode.MARKDOWN,
@@ -389,7 +389,7 @@ def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..🤗 I'm *Haguromo Otsusuki*
+            text=""" Hi..🤗 I'm *Mʀ𝙂𝘼𝙉𝙂𝙎𝙏𝙀𝙍*
                  \nHere is the [Source Code](https://GitHub.com/satyanandatripathi/emcee) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=False,
